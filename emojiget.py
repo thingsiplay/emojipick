a9161ef0ae052d0d4c6f55002e625cf1aff430ba
Tuncay D <mamehiscore@aol.com>
2021-01-24 11:58:30 +0100

    added lower case option at default

--- emojiget.py
+++ emojiget.py
@@ -9,26 +9,28 @@ import json
 import urllib.request
 import argparse
 
-jdir = pathlib.Path('~/.cache/emojiget')
-jdir = jdir.expanduser()
-jfile = pathlib.Path(jdir / 'emoji.json')
-jfile_filtered = pathlib.Path(jdir / 'emoji_filtered.json')
+cdir = pathlib.Path('~/.cache/emojiget')
+cdir = cdir.expanduser()
+jfile = pathlib.Path(cdir / 'emoji.json')
+jfile_filtered = pathlib.Path(cdir / 'emoji_filtered.json')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--filter', default='',
         help='print emoji only if name contains filter')
 parser.add_argument('-l', '--limit', default=0, type=int,
         help='limit the number of output emojis, defaults to 0 (no limit')
-parser.add_argument('-n', '--no-name', default=False, action='store_true',
+parser.add_argument('-N', '--no-name', default=False, action='store_true',
         help='do not output emoji names')
-parser.add_argument('-c', '--clean', default=False, action='store_true',
+parser.add_argument('-w', '--wipe', default=False, action='store_true',
         help='delete temporary emoji files, redownload and recreate them')
+parser.add_argument('-c', '--lower-case', default=False, action='store_true',
+        help='convert output to lower case only')
 args = parser.parse_args()
 
-if args.clean:
+if args.wipe:
     jfile.unlink(missing_ok=True)
     jfile_filtered.unlink(missing_ok=True)
-    jdir.rmdir()
+    cdir.rmdir()
     sys.exit(0)
 
 if jfile_filtered.exists():
@@ -38,7 +40,7 @@ else:
     if jfile.exists():
         jtext = jfile.read_text()
     else:
-        jdir.mkdir(exist_ok=True)
+        cdir.mkdir(exist_ok=True)
         # Source: overatgithub/emoji.json
         # Alternate = 'https://gist.githubusercontent.com/thingsiplay/1f500459bc117cf0b63e1f5c11e03963/raw/d8e4b78cfe66862cf3809443c1dba017f37b61db/emojis.json'
         jurl = 'https://gist.githubusercontent.com/oliveratgithub/0bf11a9aff0d6da7b46f1490f86a71eb/raw/d8e4b78cfe66862cf3809443c1dba017f37b61db/emojis.json'
@@ -60,5 +62,7 @@ for index, emoji in enumerate(emojis['emojis']):
         elif args.limit > 0 and index > args.limit:
             break
         output += '\n'
+if args.lower_case:
+    output = output.lower()
 if output:
     print(output)
